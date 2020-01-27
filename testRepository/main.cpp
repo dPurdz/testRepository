829f948be61a42effc8613d2500a6853e2e0c254
Dom Purdie <dompurdie97@gmail.com>
2020-01-27 17:07:49 +0000

    Added fish and salmon class

--- testRepository/main.cpp
+++ testRepository/main.cpp
@@ -7,9 +7,120 @@
 //
 
 #include <iostream>
+using namespace std;
+
+// ------- Fish Class -----------
+
+class Fish{
+    
+public:
+    
+    Fish (){
+        m_Squishiness = 0.5;
+        m_deadBool = false;
+    };
+    
+    void setSquishiness(float x){
+        m_Squishiness = x;
+    };
+    
+    float getSquishiness(){
+        return m_Squishiness;
+    };
+    
+    bool isDead(){
+        if (m_deadBool == true)
+            return true;
+        else
+            return false;
+    };
+    
+    void kill (){
+        m_deadBool = true;
+    };
+
+    
+protected:
+    
+    float m_Squishiness;
+    
+
+    
+private:
+    
+    bool m_deadBool;
+    
+};
+
+// ------- Salmon Class -----------
+
+class Salmon: public Fish{
+    
+public:
+    
+    Salmon (bool smoked){
+        m_smokedBool = false;
+    };
+    
+    bool isSmoked (){
+        if (m_smokedBool == true)
+            return true;
+        else
+            return false;
+    };
+    
+    void smokeSalmon(){
+        m_smokedBool = true;
+    };
+    
+
+    
+private:
+
+    bool m_smokedBool;
+    
+};
+
+// --------- main function ------------
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+ 
+    Salmon jeff(false);
+    
+    cout << "jeff's smoked value is: " << jeff.isSmoked() << endl;
+    
+    if (jeff.isSmoked() == true)
+        cout << "jeff is smoked" << endl;
+    else
+        cout << "jeff is unsmoked" << endl;
+    
+    
+    jeff.smokeSalmon();
+    
+    cout << "jeff's smoked value is: " << jeff.isSmoked() << endl;
+    
+    if (jeff.isSmoked() == true)
+        cout << "jeff is smoked" << endl;
+    else
+        cout << "jeff is unsmoked" << endl;
+    
+    
+    
+    cout << "jeff's squishiness value is: " << jeff.getSquishiness() << endl;
+    
+    if (jeff.isDead() == true)
+        cout << "jeff is dead" << endl;
+    else
+        cout << "jeff is alive" << endl;
+
+    
+    jeff.kill();
+    
+    if (jeff.isDead() == true)
+        cout << "jeff is dead" << endl;
+    else
+        cout << "jeff is alive" << endl;
+
+    
     return 0;
 }
